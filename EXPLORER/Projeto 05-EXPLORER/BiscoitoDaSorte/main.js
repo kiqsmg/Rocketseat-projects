bd241d049bc4c80e0af27ffb999c13719cc7a541
kiqsmg <kiqsmg@gmail.com>
2023-03-30 17:02:02 -0300

    Desafio Biscoito da sorte atualizado e corrigido falha no toggle

--- EXPLORER/Projeto 05-EXPLORER/BiscoitoDaSorte/main.js
+++ EXPLORER/Projeto 05-EXPLORER/BiscoitoDaSorte/main.js
@@ -1,20 +1,49 @@
-const button = document.querySelector('open_button')
+const screen1 = document.querySelector(".screen1")  //there is a dot before cause its a class
+const screen2 = document.querySelector(".screen2")
+const biscoito_da_sorte = document.querySelector("#biscoito_da_sorte") //there is a # before cause its a id
+const novo_biscoito_da_sorte = document.querySelector("#try_again")
 
-const Container = {
-    container: document.querySelector('container-02'),
 
-    open: function() {
-        Container.container.classList.add('open')},
-    close: function() {
-        Container.container.classList.remove('open')}
+
+const phrase = [
+    "Mais vale um na mão que dois no sutiã",
+    "Facil vem, muito dado",
+    "Pouco dado, dificil vem",
+    "Quem é amigo, amigo é",
+    "O juizo morreu de velhice",
+]
+
+
+
+biscoito_da_sorte.addEventListener('click', handleTryClick)
+novo_biscoito_da_sorte.addEventListener('click', handleResetClick)
+
+document.addEventListener('keydown', function(e) {
+    if (e.key == 'Enter' && screen2.classList.contains('hide')){
+        handleResetClick()
+    } else if(e.key == 'Enter' && screen1.classList.contains('hide')){
+        handleResetClick()
+        }
+    }
+)
+
+function handleTryClick(event) {
+    toggleScreen()
+    pickPhrase()
+
 }
 
+function handleResetClick() {
+    toggleScreen()
+}
 
+function pickPhrase() {
+    let allPhrases = phrase.length
+    let randomNumber = Math.floor(Math.random() * allPhrases)
+    screen2.querySelector("p").innerText = `${phrase[randomNumber]}`
+}
 
-
-.open_button.onclick = function(event) {
-    event.preventDefault()
-
-
-    Container.open
+function toggleScreen() {
+    screen2.classList.toggle("hide")
+    screen1.classList.toggle("hide")
 }
