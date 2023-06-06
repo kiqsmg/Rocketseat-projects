b2601fa85c5cd6a70e1572db3f2cefdb731a0eb9
kiqsmg <kiqsmg@gmail.com>
2023-06-06 15:19:29 -0300

    Add js files for the favorites code, github user code and main code

---
+++ EXPLORER/Projeto 06-EXPLORER/GitFav/assets/JS/GithubUser.js
@@ -0,0 +1,23 @@
+
+
+//Classefaz um fetch de dados no github e exporta metodo estático
+export class GithubUser {
+    static search(username) {
+
+        // Construct the API endpoint URL using the provided username
+        const endpoint = `https://api.github.com/users/${username}`
+
+
+        // Use the fetch function to make a GET request to the API endpoint
+        return fetch(endpoint)
+            // Handle the response and parse it as JSON
+            .then(data => data.json())
+            // Process the JSON data and return an object with selected properties
+            .then(data => ({
+                login: data.login,
+                name: data.name,
+                public_repos: data.public_repos,
+                followers: data.followers
+            }))
+    }
+}
