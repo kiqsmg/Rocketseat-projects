8709345ca426834ae73fee027be9856677d352dc
kiqsmg <kiqsmg@gmail.com>
2023-06-20 21:43:16 -0300

    Add gitgnore to not show the whole node_modules package

--- EXPLORER/Projeto 08-EXPLORER/API/src/server.js
+++ EXPLORER/Projeto 08-EXPLORER/API/src/server.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express(); //inicializar o express
 
+app.get("/message/:id/:user", function (request, response) {
+    const {id, user} = request.params;
+
+    response.send(`Id da mensagem: ${id}.
+    Para o usuario: ${user}.`);
+});
+
+
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`);
+});
+
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); //app fica escutando o PORT 
