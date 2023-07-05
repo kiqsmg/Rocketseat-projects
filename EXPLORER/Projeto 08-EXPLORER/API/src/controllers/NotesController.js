173318b5ee5ea6f7c964348d319fc4971879aac1
kiqsmg <kiqsmg@gmail.com>
2023-07-05 10:21:48 -0300

    End of stage8, Query Builders

--- EXPLORER/Projeto 08-EXPLORER/API/src/controllers/NotesController.js
+++ EXPLORER/Projeto 08-EXPLORER/API/src/controllers/NotesController.js
@@ -34,8 +34,72 @@ class NotesController {
         await knex("tags").insert(tagsInsert);
 
 
-        response.json();
+        return response.json();
 
+    }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const note = await knex("notes").where({ id }).first();
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at");
+
+
+        return response.json({
+            ...note,
+            tags,
+            links
+        });
+
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("notes").where({ id }).delete();
+
+        return response.json();
+    }
+
+    async index(request, response) {
+        const { title, user_id, tags } = request.query;
+
+        let notes;
+
+        if (tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("tags")
+                .select([
+                    "notes.id",
+                    "notes.title",
+                    "notes.user_id",
+                ])
+                .where("notes.user_id", user_id)
+                .whereLike("notes.title", `%${title}%`)
+                .whereIn("name", filterTags)
+                .innerJoin("notes", "notes.id", "tags.note_id")
+                .orderBy("notes.title")
+
+        } else {
+            notes = await knex("notes")
+                .where({ user_id })
+                .whereLike("title", `%${title}%`)   //em qualquer parte da frase se tiver a palavra ele ira buscar
+                .orderBy("title");
+        }
+
+        const userTags = await knex("tags").where({ user_if });
+        const notesWithTags = notes.map( note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        });
+
+        return response.json(notesWithTags);
     }
 }
 
--- EXPLORER/Projeto 08-EXPLORER/API/src/routes/index.js
+++ EXPLORER/Projeto 08-EXPLORER/API/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");   //grupo de rotas do usuario
 const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
 
 const routes = Router();
 
 // 3º Nesse local ele vai dar de cara com o /users e dizer "achei o /users que o caique quer acessar", ai vai levar a gente pro arquivo userRouter
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes;
