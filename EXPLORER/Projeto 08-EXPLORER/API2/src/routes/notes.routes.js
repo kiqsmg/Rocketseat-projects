b3b38c16bdf97a8d834a183c44d1d91009d83d8a
kiqsmg <kiqsmg@gmail.com>
2023-08-20 10:30:44 -0300

    Add new filters to search tags, notes and by title

--- EXPLORER/Projeto 08-EXPLORER/API2/src/routes/notes.routes.js
+++ EXPLORER/Projeto 08-EXPLORER/API2/src/routes/notes.routes.js
@@ -9,7 +9,12 @@ const notesController = new NotesController();
 
 
 //Query Params  ... não é obrigatorio ter os parametros para abrir a rota!
+
+notesRoutes.get("/", notesController.index);
 notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.get("/:id", notesController.show);
+notesRoutes.delete("/:id", notesController.delete);
+
 
 
 //exportar userRoutes 
