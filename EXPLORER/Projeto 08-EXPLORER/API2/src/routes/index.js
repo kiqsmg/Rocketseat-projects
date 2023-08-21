685efe91b6689ef6f3229a693877d93f3b2415da
kiqsmg <kiqsmg@gmail.com>
2023-08-21 15:13:08 -0300

    Final changes in the backend module folders and files

--- EXPLORER/Projeto 08-EXPLORER/API2/src/routes/index.js
+++ EXPLORER/Projeto 08-EXPLORER/API2/src/routes/index.js
@@ -4,11 +4,13 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");    //grupo de rotas do usuario
 const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
 
 
 const routes = Router();
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 
 module.exports = routes;
