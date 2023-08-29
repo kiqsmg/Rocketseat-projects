2cb2c839bfe8d9a94437ff5a209a95bb0df2af2c
kiqsmg <kiqsmg@gmail.com>
2023-08-29 12:34:32 -0300

    Criando novas funcionalidades excenciais para o server o projeto

--- EXPLORER/Projeto 08-EXPLORER/Node_movies/src/server.js
+++ EXPLORER/Projeto 08-EXPLORER/Node_movies/src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+app.get("/users", (request, response) => {
+    const { name, email, password } = request.body;
+
+    response.send(`Usuario: ${name}, Email: ${email}, senha: ${password}`);
+});
 
 
 const PORT = 3333;
