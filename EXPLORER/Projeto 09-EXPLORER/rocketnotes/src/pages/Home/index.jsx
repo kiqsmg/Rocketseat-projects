764ee19dac71f95591928d44a3f6d7f2a9107394
kiqsmg <kiqsmg@gmail.com>
2023-09-02 20:53:30 -0300

    Create Input module

--- EXPLORER/Projeto 09-EXPLORER/rocketnotes/src/pages/Home/index.jsx
+++ EXPLORER/Projeto 09-EXPLORER/rocketnotes/src/pages/Home/index.jsx
@@ -1,7 +1,10 @@
+import { FiPlus, FiSearch } from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
+import { Input } from '../../components/Input';
 
 
 
@@ -23,7 +26,7 @@ export function Home() {
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
             </Search>
 
             <Content>
