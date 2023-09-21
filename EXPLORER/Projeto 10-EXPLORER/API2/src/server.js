d1e8b1cdac45c61a5941c1233cefd63ee50df3e0
kiqsmg <kiqsmg@gmail.com>
2023-09-21 13:31:09 -0300

    Fix database problems

--- EXPLORER/Projeto 10-EXPLORER/API2/src/server.js
+++ EXPLORER/Projeto 10-EXPLORER/API2/src/server.js
@@ -3,6 +3,7 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require("express");   //importou express
 const routes = require("./routes");  //se tu nao especifica qual file dentro de routes ele automaticamnte abre o index
 
@@ -11,6 +12,7 @@ migrationsRun();  //executar o banco de dados
 
 
 const app = express();  //vai atender solicitações
+app.use(cors());  //permite o backend atender as requisições do frontend
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
